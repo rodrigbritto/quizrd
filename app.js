498e61a8492bc1125bbc562c6d2e0908c1104d16
Rodrigo Brito <97199889+rodrigbritto@users.noreply.github.com>
2022-07-11 02:01:12 -0300

    Commit Inicial

---
+++ app.js
@@ -0,0 +1,459 @@
+var quiz = {  
+    "JS": [  
+      { 
+        "id": 1,  
+        "question": "Qual meu nome completo?",  
+        "options": [  
+          {  
+            "a": "Rodrigo Ferreira Monteiro De Brito ",  
+            "b": "Rodrigo Geraldo Ferreira Monteiro De Brito",  
+            "c": "Rodrigo Geraldo Monteiro De Brito Junior",  
+            "d": "Rodrigo Geraldo Ferreira Monteiro De Brito Junior"  
+          }  
+        ],  
+        "answer": "Rodrigo Geraldo Ferreira Monteiro De Brito Junior",  
+        "score": 0,  
+        "status": ""  
+      },  
+      {  
+        "id": 2,  
+        "question": "Qual meu signo?",  
+        "options": [  
+          {  
+            "a": "Peixes",  
+            "b": "Aries",  
+            "c": "Libras"  
+          }  
+        ],  
+        "answer": "Peixes",  
+        "score": 0,  
+        "status": ""  
+      },  
+      {  
+        "id": 3,  
+        "question": "Qual o Mes do meu aniversario?",  
+        "options": [  
+          {  
+            "a": "Maio",  
+            "b": "Março",  
+            "c": "Fevereiro"  
+          }  
+        ],  
+        "answer": "Março",  
+        "score": 0,  
+        "status": ""  
+      },  
+      {  
+        "id": 4,  
+        "question": "É verdade que eu trocaria festa, por uma partida de call of duty?",  
+        "options": [  
+          {  
+            "a": "Verdade",  
+            "b": "Mentira"  
+          }  
+        ],  
+        "answer": "Verdade",  
+        "score": 0,  
+        "status": ""  
+      },  
+      {  
+        "id": 5,  
+        "question": "Qual meu jogo favorito?",  
+        "options": [  
+          {  
+            "a": "Resident Evil",  
+            "b": "Cs go",  
+            "c": "Call of duty",  
+            "d": "Gta v",  
+          }  
+        ],  
+        "answer": "Call of duty",  
+        "score": 0,  
+        "status": ""  
+      },  
+      {  
+        "id": 6,  
+        "question": "Qual meu youtuber favorito?",  
+        "options": [  
+          {  
+            "a": "Luan Gameplay",  
+            "b": "FunkyblackCat",  
+            "c": "Voce Sabia",  
+          }  
+        ],  
+        "answer": "Luan Gameplay",  
+        "score": 0,  
+        "status": ""  
+      },  
+      {  
+        "id": 7,  
+        "question": "Qual minha comida favorita?",  
+        "options": [  
+          {  
+            "a": "Strognoff",  
+            "b": "Lasanha",  
+            "c": "Macarrão",  
+          }  
+        ],  
+        "answer": "Strognoff",  
+        "score": 0,  
+        "status": ""  
+      },  
+      {  
+        "id": 8,  
+        "question": "Qual minha bebida favorita?",  
+        "options": [  
+          {  
+            "a": "Vodka",  
+            "b": "Cerveja",  
+            "c": "Vinho",  
+            "d": " cachaça",  
+          }  
+        ],  
+        "answer": "cachaça",  
+        "score": 0,  
+        "status": ""  
+      },  
+      {  
+        "id": 9,  
+        "question": "Qual faculdade eu curso?",  
+        "options": [  
+          {  
+            "a": "Tecnologia da informação",  
+            "b": "Desenvolvedor website",
+            "c": "Desenvolvedor de software",  
+           "d": "Ciência da computação"  
+         }  
+       ],  
+       "answer": "Desenvolvedor de software",  
+       "score": 0,  
+       "status": ""  
+     },  
+     {  
+       "id": 10,  
+       "question": "Qual minha cor favorita?",  
+       "options": [  
+         {  
+           "a": "preto",  
+           "b": "azul",  
+           "c": "verde",  
+           "d": "rosa",  
+         }  
+       ],  
+       "answer": "verde",  
+       "score": 0,  
+       "status": ""  
+     },  
+     {  
+       "id": 11,  
+       "question": "qual a cor dos meus olhos?",  
+       "options": [  
+         {  
+           "a": "Verde",  
+           "b": "Azul",  
+           "c": "Castanho"  
+         }  
+       ],  
+       "answer": "verde",  
+       "score": 0,  
+       "status": ""  
+     },  
+     {  
+       "id": 12,  
+       "question": "qual desses esportes eu gosto de fazer?",  
+       "options": [  
+         {  
+           "a": "skate",  
+           "b": "surf",  
+           "c": "muay thai",  
+           "d": "volei"  
+         }  
+       ],  
+       "answer": "skate",  
+       "score": 0,  
+       "status": ""  
+     },  
+     {  
+       "id": 13,  
+       "question": "onde eu nasci?",  
+       "options": [  
+         {  
+           "a": "Rio de janeiro",  
+           "b": "São Gonçalo",  
+           "c": "Rio das ostras"  
+         }  
+       ],  
+       "answer": "São Gonçalo",  
+       "score": 0,  
+       "status": ""  
+     },  
+     {  
+       "id": 14,  
+       "question": "qual meu segundo idioma favorito?",  
+       "options": [  
+         {  
+           "a": "ingles",  
+           "b": "espanhol",  
+           "c": "francês"  
+         }  
+       ],  
+       "answer": "francês",  
+       "score": 0,  
+       "status": ""  
+     },  
+     {  
+       "id": 15,  
+       "question": "minha marca favorita?",  
+       "options": [  
+         {  
+           "a": "nike",  
+           "b": "adidas",
+           "c": "vans",  
+           "d": "hurley"  
+         }  
+       ],  
+       "answer": "nike",  
+       "score": 0,  
+       "status": ""  
+     },  
+     {  
+       "id": 16,  
+       "question": "qual meu animal favorito?",  
+       "options": [  
+         {  
+           "a": "cachorro",  
+           "b": "gato",  
+           "c": "golfinho",  
+           "d": "lhama"  
+         }  
+       ],  
+       "answer": "lhama",  
+       "score": 0,  
+       "status": ""  
+     },  
+     {  
+       "id": 17,  
+       "question": "qual dessas empress me deu mais estresse?",  
+       "options": [  
+         {  
+           "a": "prepara cursos",  
+           "b": "shop15",  
+           "c": "casas bahia",  
+           "d": "ponto frio"  
+         }  
+       ],  
+       "answer": "prepara curso",  
+       "score": 0,  
+       "status": ""  
+     },  
+     {  
+       "id": 18,  
+       "question": "qual desses dois app eu gosto mais!",  
+       "options": [  
+         {  
+           "a": "tiktok",  
+           "b": "facebook"  
+         }  
+       ],  
+       "answer": "facebook",  
+       "score": 0,  
+       "status": ""  
+     },  
+     {  
+       "id": 19,  
+       "question": "é verdade que eu ja tive depressão?",  
+       "options": [  
+         {  
+           "a": "verdade",  
+           "b": "falso"  
+         }  
+       ],  
+       "answer": "verdade",  
+       "score": 0,  
+       "status": ""  
+     },  
+     {  
+       "id": 20,  
+       "question": "qual profissão eu gostaria de se?",  
+       "options": [  
+         {  
+           "a": "vendedor",  
+           "b": "programador",  
+           "c": "off shore"  
+         }  
+       ],  
+       "answer": "programador",  
+       "score": 0,  
+       "status": ""  
+     },  
+     {  
+       "id": 21,  
+       "question": "qual dessas linguagens de programção eu entendo mais?",  
+       "options": [  
+         {  
+           "a": "javascript",  
+           "b": "python",  
+           "c": "c#",  
+           "d": "C++"  
+         }  
+       ],  
+       "answer": "javascript",  
+       "score": 0,  
+       "status": ""  
+     },  
+     {  
+       "id": 22,  
+       "question": "qul minha fruta favorita",  
+       "options": [  
+         {  
+           "a": "banana",  
+           "b": "morango",  
+           "c": "uva"  
+         }  
+       ],  
+       "answer": "banana",  
+       "score": 0,  
+       "status": ""  
+     },  
+     {  
+       "id": 23,  
+       "question": "qual minha atriz porno favorita kkkkk?",  
+       "options": [  
+         {  
+           "a": "lana rhoades",  
+           "b": "emme white",  
+           "c": "mia khalifa",  
+           "d": "elisa sanches"  
+         }  
+       ],  
+       "answer": "mia khalifa",  
+       "score": 0,  
+       "status": ""  
+     },  
+     {  
+       "id": 24,  
+       "question": "qual minha religião?",  
+       "options": [  
+         {  
+           "a": "não tem",  
+           "b": "cristão",  
+           "c": "espírita"  
+         }  
+       ],  
+       "answer": "não tem",  
+       "score": 0,  
+       "status": ""  
+     },  
+     {  
+       "id": 25,  
+       "question": "eu acredito em Deus?",  
+       "options": [  
+         {  
+           "a": "não",  
+           "b": "sim"  
+         }  
+       ],  
+       "answer": "sim",  
+       "score": 0,  
+       "status": ""  
+     }  
+   ]  
+ }  
+ var quizApp = function () {  
+   this.score = 0;  
+   this.qno = 1;  
+   this.currentque = 0;  
+   var totalque = quiz.JS.length;  
+   this.displayQuiz = function (cque) {  
+     this.currentque = cque;  
+     if (this.currentque < totalque) {  
+       $("#tque").html(totalque);  
+       $("#previous").attr("disabled", false);  
+       $("#next").attr("disabled", false);  
+       $("#qid").html(quiz.JS[this.currentque].id + '.');  
+       $("#question").html(quiz.JS[this.currentque].question);  
+       $("#question-options").html("");  
+       for (var key in quiz.JS[this.currentque].options[0]) {  
+         if (quiz.JS[this.currentque].options[0].hasOwnProperty(key)) {  
+           $("#question-options").append(  
+             "<div class='form-check option-block'>" +  
+             "<label class='form-check-label'>" +  
+             "<input type='radio' class='form-check-input' name='option'  id='q" + key + "' value='" + quiz.JS[this.currentque].options[0][key] + "'><span id='optionval'>" +  
+             quiz.JS[this.currentque].options[0][key] +  
+             "</span></label>"  
+           );  
+         }  
+       }  
+     }  
+     if (this.currentque <= 0) {  
+       $("#previous").attr("disabled", true);  
+     }  
+     if (this.currentque >= totalque) {  
+       $('#next').attr('disabled', true);  
+       for (var i = 0; i < totalque; i++) {  
+         this.score = this.score + quiz.JS[i].score;  
+       }  
+       return this.showResult(this.score);  
+     }  
+   }  
+   this.showResult = function (scr) {  
+     $("#result").addClass('result');  
+     $("#result").html("<h1 class='res-header'>Total Score: &nbsp;" + scr + '/' + totalque + "</h1>");  
+     for (var j = 0; j < totalque; j++) {  
+       var res;  
+       if (quiz.JS[j].score == 0) {  
+         res = '<span class="wrong">' + quiz.JS[j].score + '</span><i class="fa fa-remove c-wrong"></i>';  
+       } else {  
+         res = '<span class="correct">' + quiz.JS[j].score + '</span><i class="fa fa-check c-correct"></i>';  
+       }  
+       $("#result").append(  
+         '<div class="result-question"><span>Q ' + quiz.JS[j].id + '</span> &nbsp;' + quiz.JS[j].question + '</div>' +  
+         '<div><b>Correct answer:</b> &nbsp;' + quiz.JS[j].answer + '</div>' +
+         '<div class="last-row"><b>Score:</b> &nbsp;' + res +  
+         '</div>'  
+       );  
+     }  
+   }  
+   this.checkAnswer = function (option) {  
+     var answer = quiz.JS[this.currentque].answer;  
+     option = option.replace(/</g, "&lt;")  //for <  
+     option = option.replace(/>/g, "&gt;")  //for >  
+     option = option.replace(/"/g, "&quot;")  
+     if (option == quiz.JS[this.currentque].answer) {  
+       if (quiz.JS[this.currentque].score == "") {  
+         quiz.JS[this.currentque].score = 1;  
+         quiz.JS[this.currentque].status = "correct";  
+       }  
+     } else {  
+       quiz.JS[this.currentque].status = "wrong";  
+     }  
+   }  
+   this.changeQuestion = function (cque) {  
+     this.currentque = this.currentque + cque;  
+     this.displayQuiz(this.currentque);  
+   }  
+ }  
+ var jsq = new quizApp();  
+ var selectedopt;  
+ $(document).ready(function () {  
+   jsq.displayQuiz(0);  
+   $('#question-options').on('change', 'input[type=radio][name=option]', function (e) {  
+     //var radio = $(this).find('input:radio');  
+     $(this).prop("checked", true);  
+     selectedopt = $(this).val();  
+   });  
+ });  
+ $('#next').click(function (e) {  
+   e.preventDefault();  
+   if (selectedopt) {  
+     jsq.checkAnswer(selectedopt);  
+   }  
+   jsq.changeQuestion(1);  
+ });  
+ $('#previous').click(function (e) {  
+   e.preventDefault();  
+   if (selectedopt) {  
+     jsq.checkAnswer(selectedopt);  
+   }  
+   jsq.changeQuestion(-1);  
+ });
